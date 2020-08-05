6b1ec14b6c81bd859361f2bb1cb11dca126e517e
Max Czapanskiy <maxczap@stanford.edu>
2020-08-04 17:01:48 -0700

    fix pkg install

--- MAKE.R
+++ MAKE.R
@@ -1,12 +1,19 @@
 # Required packages
-pkgs <- c("ape",
-          "fasterize",
-          "picante",
-          "raster",
-          "rotl",
-          "sf",
-          "tidyverse")
-install.packages(pkgs)
+pkgTest <- function(x) {
+  if (!require(x, character.only = TRUE)) {
+    install.packages(x, dep = TRUE)
+    if(!require(x, character.only = TRUE)) {
+      stop("Package not found")
+    }
+  }
+}
+pkgTest("ape")
+pkgTest("fasterize")
+pkgTest("picante")
+pkgTest("raster")
+pkgTest("rotl")
+pkgTest("sf")
+pkgTest("tidyverse")
 
 # Check that packages were installed
 if (any(!pkgs %in% installed.packages())) {
@@ -17,6 +24,6 @@ if (any(!pkgs %in% installed.packages())) {
 marmam_shp <- sf::st_read("data/marmam.shp", quiet = TRUE)
 marmam_tr <- ape::read.tree("data/marmam.nex")
 coastline_shp <- sf::st_read("data/coastline.shp", quiet = TRUE)
-if (any(c("marmam_shp", "marmam_tr", "coastline_shp") %in% ls())) {
+if (any(!c("marmam_shp", "marmam_tr", "coastline_shp") %in% ls())) {
   cat("Data load error\n", file = "err.txt", append = TRUE)
 }
